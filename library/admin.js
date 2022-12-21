87fd16f4e41ebb6dee0d624ff939a9533ffee510
Gokul_Kathirvel <gokulyes25@gmail.com>
2022-12-21 17:40:14 +0530

    populated overall score and  filtered by sorting

--- library/admin.js
+++ library/admin.js
@@ -50,13 +50,33 @@ exports.getEmployeeLeaderBasedOnCriteria = async (params) => {
     } catch (err) {
         throw err
     }
-    return parseEmployeeLeaderBasedOnCriteria(result);
+    return parseEmployeeLeaderBasedOnCriteria(result,params);
 }
 
 
-const parseEmployeeLeaderBasedOnCriteria = (result)=>{
-    if(result && Array.isArray(result) && result.length > 0){
-        
+const parseEmployeeLeaderBasedOnCriteria = (result, params) => {
+    let employeeDtls = {}
+    if (result && Array.isArray(result) && result.length > 0) {
+        employeeDtls = result.reduce((ack, curr) => {
+            ack[curr.emp_id] = ack[curr.emp_id] || {}
+            ack[curr.emp_id]['emp_id'] = curr.emp_id;
+            ack[curr.emp_id]['emp_name'] = curr.emp_name;
+            ack[curr.emp_id]['score'] = ack[curr.emp_id]['score'] ? (parseFloat(ack[curr.emp_id]['score']) + parseFloat(curr.score)) : parseFloat(curr.score);
+            ack[curr.emp_id]['gender'] = curr.gender;
+            return ack
+        }, {})
+        let fResult = Object.values(employeeDtls);
+        if (fResult && Array.isArray(fResult) && fResult.length > 0) {
+            let sortBy = (params['filterParams'] && params['filterParams']['sortBy']) ? params['filterParams']['sortBy'] : "DESC"
+            switch (sortBy) {
+                case "ASC":
+                    fResult.sort((a, b) => a.score - b.score);
+                    break;
+                default:
+                    fResult.sort((a, b) => -1 * (a.score - b.score));
+            }
+        }
+        return fResult;
     }
 }
 
